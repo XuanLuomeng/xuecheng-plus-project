5db1020daa698918e18307fd5851997b7148136a
luojiawei <1079716693@qq.com>
2024-03-06 22:26:06 +0800

    2024-3-6

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
@@ -0,0 +1,19 @@
+package com.xuecheng;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LuoXuanwei
+ * @version 1.0
+ * @description 内容管理服务启动类
+ * @date 2024/3/5 21:39
+ */
+@EnableSwagger2Doc
+@SpringBootApplication
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class, args);
+    }
+}
